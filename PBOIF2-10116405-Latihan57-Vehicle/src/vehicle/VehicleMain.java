c03d036c36e788a04fd142262be02c2873faec27
Fahrih Fatahilah <painchikusudo@gmail.com>
2020-11-22 13:21:55 +0800

    Add files via upload

---
+++ PBOIF2-10116405-Latihan57-Vehicle/src/vehicle/VehicleMain.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vehicle;
+
+/**
+ *
+ * @author Fahrih Fatahilah  
+ */
+public class VehicleMain {
+
+ /**
+ *Nama  : Fahrih Fatahilah
+ *Kelas : PBO2
+ *NIM   : 10116405
+ *Deskripso Program : program ini berisi program yang menampilkan konstruktor
+ *                    dari Vehicle
+ */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          Bicycle bicycle = new Bicycle();
+        Skateboard skateboard = new Skateboard();
+    }
+    
+}
